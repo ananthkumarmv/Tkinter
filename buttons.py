6a647878fe034091e5972e669324e12542bc39bf
Ananth <mvananthkumar@gmail.com>
2021-12-20 14:21:44 +0530

    Create buttons.py

---
+++ buttons.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+def myClick():
+	myLabel = Label(root, text = "Look I Clicked a Button!!")
+	myLabel.pack()
+
+
+myButton = Button(root, text = "Click Me!", command=myClick)  # , state = DISABLED, padx=50, pady=50
+myButton.pack()
+
+root.mainloop()
