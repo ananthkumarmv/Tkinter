f119eb78d5b70ae1367ac961152cca6bf1fbe88d
Ananth <mvananthkumar@gmail.com>
2021-12-20 22:34:58 +0530

    Update viewer.py

--- viewer.py
+++ viewer.py
@@ -27,14 +27,40 @@ def forward(image_number):
 	global button_back
 
 	my_label.grid_forget()
+	my_label = Label(image=image_list[image_number-1])
+	button_forward = Button(root, text=">>", command = lambda: forward(image_number+1))
+	button_back = Button(root, text = "<<", command=lambda: back(image_number-1))
 
-def back():
-	return
+	if image_number == 5:
+		button_forward = Button(root, text=">>", state=DISABLED)
+
+	my_label.grid(row=0, column=0, columnspan=3)
+	button_back.grid(row=1, column=0)
+	button_forward.grid(row=1, column=2)
 
 
 
+def back(image_number):
+	global my_label
+	global button_forward
+	global button_back
 
-button_back = Button(root, text='<<', command=back)
+	my_label.grid_forget()
+	my_label = Label(image=image_list[image_number-1])
+	button_forward = Button(root, text=">>", command = lambda: forward(image_number+1))
+	button_back = Button(root, text = "<<", command=lambda: back(image_number-1))
+
+	if image_number == 1:
+		button_forward = Button(root, text="<<", state=DISABLED)
+
+
+	my_label.grid(row=0, column=0, columnspan=3)
+	button_back.grid(row=1, column=0)
+	button_forward.grid(row=1, column=2)
+
+
+
+button_back = Button(root, text='<<', command=back, state=DISABLED)
 button_exit = Button(root, text='Exit', command=root.quit)
 button_forward = Button(root, text='>>', command= lambda: forward(2))
 
