968ac727d6d668a434d0a88b5b2d13b1a3226c9c
Ananth <mvananthkumar@gmail.com>
2021-12-23 15:28:52 +0530

    Update radio-button.py

--- radio-button.py
+++ radio-button.py
@@ -10,20 +10,35 @@ r = IntVar()
 r.set("2")
 
 
+TOPPINGS = [
+	("Pepperoni", "Pepperoni"),
+	("Cheese", "Cheese"),
+	("Mushroom", "Mushroom"),
+	("Onion", "Onion")
+]
+
+pizza = StringVar()
+pizza.set("Pepperoni")
+
+for text, topping, in TOPPINGS:
+	Radiobutton(root, text=text, variable=pizza, value=topping).pack(anchor=W)
+
+
+
 def clicked(value):
 	myLable = Label(root, text=value)
 	myLable.pack()
 
 
-Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: clicked(r.get())).pack()
-Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: clicked(r.get())).pack()
+# Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: clicked(r.get())).pack()
+# Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: clicked(r.get())).pack()
 
-myLable = Label(root, text=r.get())
-myLable.pack()
+# myLable = Label(root, text=pizza.get())
+# myLable.pack()
 
 
 
-myButton = Button(root, text="Click Me!", command=lambda: clicked(r.get())).pack()
+myButton = Button(root, text="Click Me!", command=lambda: clicked(pizza.get())).pack()
 
 
 root.mainloop()
