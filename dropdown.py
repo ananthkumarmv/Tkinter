620f11c7250f7fa3cf931238031aa64f66b53fdc
Ananth <mvananthkumar@gmail.com>
2021-12-31 23:45:08 +0530

    Create dropdown.py

---
+++ dropdown.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+root = Tk()
+root.title("Title goes here")
+root.iconbitmap('Images/codemy.ico')
+root.geometry("400x400")
+
+# Drop Down Boxes
+
+drop
+
+
+
+
+
+root.mainloop()
