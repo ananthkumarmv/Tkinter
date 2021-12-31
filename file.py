11e7417c87735dd2034911a161f7d84e52568daf
Ananth <mvananthkumar@gmail.com>
2021-12-31 21:43:09 +0530

    Update file.py

--- file.py
+++ file.py
@@ -6,9 +6,14 @@ root = Tk()
 root.title("Title goes here")
 root.iconbitmap('Images/codemy.ico')
 
-root.filename = filedialog.askopenfilename(initialdir="../Tkinter/Images", title="Select A File", filetypes=(("png files", "*.png"), ("jpg files", "*.jpg"), ("all files", "*.*")))
-my_label = Label(root, text=root.filename).pack()
-my_image = ImageTk.PhotoImage(Image.open(root.filename))
-my_image_label = Label(image = my_image).pack()
 
+def open():
+	global my_image
+	root.filename = filedialog.askopenfilename(initialdir="../Tkinter/Images", title="Select A File", filetypes=(("png files", "*.png"), ("jpg files", "*.jpg"), ("all files", "*.*")))
+	my_label = Label(root, text=root.filename).pack()
+	my_image = ImageTk.PhotoImage(Image.open(root.filename))
+	my_image_label = Label(image = my_image).pack()
+
+
+my_btn = Button(root, text = "Open File", command=open).pack()
 root.mainloop()
