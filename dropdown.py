6aad67a98ae66199b91c42f61afaf205b5a4bf21
Ananth <mvananthkumar@gmail.com>
2021-12-31 23:51:18 +0530

    Update dropdown.py

--- dropdown.py
+++ dropdown.py
@@ -9,10 +9,28 @@ root.geometry("400x400")
 
 # Drop Down Boxes
 
-drop
+def show():
+	myLabel = Label(root, text=clicked.get()).pack()
 
 
+options = [
+			"Mon", 
+			"Tue", 
+			"Wed", 
+			"Thu", 
+			"Fri"
+]
 
+clicked = StringVar()
+clicked.set(options[0])
+
+
+drop = OptionMenu(root, clicked, *options)
+drop.pack()
+
+
+myButton = Button(root, text="Show Selection", command=show).pack()
 
 
 root.mainloop()
+ 
